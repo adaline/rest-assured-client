4a9c7f6269e38f63742e9c0a58afe82a04d3e823
Val <hello@madespecial.co.uk>
2016-10-12 21:38:28 +0100

    Better codes

---
+++ rest_assured.rb
@@ -0,0 +1,48 @@
+require 'rest-client'
+require 'json'
+
+# For list of rest-client methods see:
+# https://github.com/rest-client/rest-client/blob/master/lib/restclient.rb#L64
+
+class RestAssured
+  def initialize(url, secret_key)
+    @secret_key = secret_key
+    @url = url
+  end
+
+  def accounts
+    RestClient.get(
+      "#{@url}/accounts",
+      headers
+    )
+  end
+
+  def transactions
+    RestClient.get(
+      "#{@url}/transactions",
+      headers
+    )
+  end
+
+  def create_transaction(destination, quantity)
+    begin
+     RestClient.post(
+        "#{@url}/transactions",
+        { destination: destination, quantity: quantity }.to_json,
+        headers
+      )
+     rescue RestClient::ExceptionWithResponse => e
+     e.response
+   end
+  end
+
+  private
+
+  def headers
+    { authorization: @secret_key, content_type: :json }
+  end
+end
+
+# require './rest_assured'
+# rest_assured = RestAssured.new('http://xxxx', 'xxxx')
+# puts rest_assured.accounts
